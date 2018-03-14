4b3396a9dd44540d240de86cb732e317d2bd5839
Roman Pozdniakov <roman.pozdniakov@arcadia.spb.ru>
2018-03-14 16:31:56 +0300

    first commit

---
+++ view1/view1.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+    .controller('View1Ctrl', function ($scope, $http) {
+        $scope.user = {};
+
+        $scope.login = function(){
+            $http.post('login', $scope.user)
+                .then(() => {
+                    console.log('success');
+                },
+                () => { console.log('fail');});
+        }
+});
